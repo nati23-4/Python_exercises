ee1c1babed08b2a42985c384fb53fac91fbbb9aa
Natalie Hannoush <nhannoush@login-01.cm.cluster>
2025-09-04 16:07:01 -0400

    Add Collatz

---
+++ Practice.py
@@ -0,0 +1,13 @@
+#Practice exercises for practice exercises
+#:insert crying emoji
+#9/2/2025
+#9:41
+
+n = 5
+while n > 0:
+    n = n-1
+    if n ==2:
+        break
+    print(n)
+print("All done")
+
